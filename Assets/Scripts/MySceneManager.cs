2bcf54ab8c55919362923a53ba2bbd4e40a769d6
Juan Miguel <kitsu_pkiller@hotmail.com>
2020-11-11 22:43:09 +0100

    Added some plugin for edit texts as button. Added some scenes and MySceneManager. Coded the MySceneManager to pass throw scenes and make the menu interactive. Added a temporal Menu to work on it.

---
+++ Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour
+{
+    public static MySceneManager istance = null;
+
+    private void Awake()
+    {
+        istance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //This method load the scene of the base. 
+    public void Base()
+    {
+        SceneManager.LoadScene("BaseScene");
+    }
+    
+    //This method load the game scene.
+    public void Game()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
